4c9d2f715840b7d9f8ca440fabbe91bcd6c35c4b
Fabricio <39932090+Fabri31295@users.noreply.github.com>
2020-09-29 04:15:32 -0300

    Finalizacion de tablas y creacion de usuario admin

--- parquimetros.sql
+++ parquimetros.sql
@@ -1,6 +1,8 @@
 # Autores: Almaraz Fabricio, Pacione Luciano
 
 
+# HAY QUE CORREGIR LO DE FECHAS Y HORAS PORQUE NO SE COMO VA, EN EL PRÁCTICO AL FINAL DICE
+
 
 # Creacion de base de datos.
 CREATE database parquimetros;
@@ -32,8 +34,10 @@ CREATE TABLE automoviles (
   dni INT unsigned NOT NULL,
 
   CONSTRAINT pk_automoviles
-  PRIMARY KEY (patente)
+  PRIMARY KEY (patente),
+
   FOREIGN KEY (dni) REFERENCES conductores(dni)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
 
 ) ENGINE=InnoDB;
 
@@ -55,8 +59,12 @@ CREATE TABLE tarjeta (
   patente VARCHAR(30) NOT NULL,
 
   CONSTRAINT pk_tarjeta
-  PRIMARY KEY (id_tarjeta)
-  FOREIGN KEY (tipo,patente) REFERENCES tipos_tarjeta(tipo), automoviles(patente)
+  PRIMARY KEY (id_tarjeta),
+
+  FOREIGN KEY (tipo) REFERENCES tipos_tarjeta(tipo)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (patente) REFERENCES automoviles(patente)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
 
 ) ENGINE=InnoDB;
 
@@ -92,8 +100,10 @@ CREATE TABLE parquimetros (
   altura INT unsigned NOT NULL,
 
   CONSTRAINT pk_parquimetros
-  PRIMARY KEY (id_parq)
+  PRIMARY KEY (id_parq),
+
   FOREIGN KEY (calle,altura) REFERENCES ubicaciones(calle,altura)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
 
 ) ENGINE=InnoDB;
 
@@ -101,15 +111,18 @@ CREATE TABLE parquimetros (
 CREATE TABLE estacionamientos (
   id_tarjeta INT unsigned NOT NULL,
   id_parq INT unsigned NOT NULL,
-  #fecha_ent
-  #hora_ent
-  #fecha_sal
-  #hora_sal
+  fecha_ent = DATE,
+  hora_ent = TIME,
+  fecha_sal = DATE,
+  hora_sal = TIME,
 
   CONSTRAINT pk_estacionamientos
-  PRIMARY KEY (id_parq,fecha_ent,hora_ent)
+  PRIMARY KEY (id_parq,fecha_ent,hora_ent),
+
   FOREIGN KEY (id_parq) REFERENCES parquimetros(id_parq)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
   FOREIGN KEY (id_tarjeta) REFERENCES tarjeta(id_tarjeta)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
 
 ) ENGINE=InnoDB;
 
@@ -117,28 +130,74 @@ CREATE TABLE estacionamientos (
 CREATE TABLE accede (
   legajo  INT unsigned NOT NULL,
   id_parq INT unsigned NOT NULL,
-  #fecha
-  #hora
+  fecha = DATE,
+  hora = TIME,
 
   CONSTRAINT pk_accede
-  PRIMARY KEY (id_parq,fecha,hora)
+  PRIMARY KEY (id_parq,fecha,hora),
+
   FOREIGN KEY (legajo) REFERENCES inspectores (legajo)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
   FOREIGN KEY (id_parq) REFERENCES parquimetros (id_parq)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
 
 ) ENGINE=InnoDB;
 
 
 CREATE TABLE asociado_con (
+  id_asociado_con INT unsigned NOT NULL,
+  legajo INT unsigned NOT NULL,
+  calle VARCHAR(50) NOT NULL,
+  altura INT unsigned NOT NULL,
+  dia VARCHAR(20) NOT NULL,
+  turno VARCHAR(1) NOT NULL,
+
+  CONSTRAINT pk_asociado_con
+  PRIMARY KEY (id_asociado_con),
+
+  FOREIGN KEY (legajo) REFERENCES inspectores (legajo)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (calle,altura) REFERENCES ubicaciones (calle,altura)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
 
 ) ENGINE=InnoDB;
 
 
 CREATE TABLE multa (
+  numero INT unsigned NOT NULL,
+  fecha = DATE,
+  hora = TIME,
+  patente VARCHAR(30) NOT NULL,
+  id_asociado_con INT unsigned NOT NULL,
+
+  CONSTRAINT pk_multa
+  PRIMARY KEY (numero),
+
+  FOREIGN KEY (patente) REFERENCES automoviles (patente)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (id_asociado_con) REFERENCES asociado_con (id_asociado_con)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
 
 ) ENGINE=InnoDB;
 
 
 
+# -----------------------------------------------------------------------------
+  # Funcion hash para nombre de usuario y password
+  #insert into inspectores values('u1', md5('pw1'))
+
+
+# -----------------------------------------------------------------------------
+# Creacio de usuario administrador
+
+CREATE USER admin@localhost IDENTIFIED BY PASSWORD 'admin'
+
+GRANT ALL PRIVILEGES ON parquimetros.* TO admin@localhost WITH GRANT OPTION;
+
+# -----------------------------------------------------------------------------
+# Creacion de otros usuarios
+
+CREATE USER venta@'%' IDENTIFIED BY PASSWORD 'venta'
 
 
 
