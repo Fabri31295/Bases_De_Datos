dc1bf1226ded6b5446d6e389b8b94e0d68063ca8
Fabricio <39932090+Fabri31295@users.noreply.github.com>
2020-10-16 23:40:09 -0300

    Java

---
+++ Java/src/Main.java
@@ -0,0 +1,29 @@
+// Almaraz Fabricio, Pacione Luciano
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import quick.dbtable.DBTable;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@SuppressWarnings("serial")
+public class Main extends JFrame{
+
+	//private MainPanel contentPane;
+	//private JTextField tFieldUser;
+	//private JLabel lblUser, lblPassword;
+	//private JPasswordField passwordField;
+	//private JButton btnLogin, btnCancel;
+	//private DataBaseConnection connection;
+	//private JMenuItem mntmLogout;
+	//private static JFrame INSTANCE;
+	
+	public static void main(String args[]) {
+		
+		
+	}
+	
+}
